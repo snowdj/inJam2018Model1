38776c49844fd231243fcb12601d0063c589d0fe
udothemath1984 <anappleaday1984@gmail.com>
2018-07-26 02:40:35 -0400

    indention

--- app.py
+++ app.py
@@ -14,27 +14,27 @@ def homepage():
 
 @app.route('/model1', methods=['POST'])
 def make_predict():
-	myPickle = 'model.pkl'
-	encoder, modelCatBoost = pickle.load(open(myPickle, 'rb'))
-	## all kinds of error checking should go here
-    data = request.get_json(force=True)
-	## convert our json to a numpy array 
-	predict_request = [data['CnaeSession'], 
+    myPickle = 'model.pkl'
+    encoder, modelCatBoost = pickle.load(open(myPickle, 'rb'))
+    ## all kinds of error checking should go here
+    ## convert our json to a numpy array  
+    data = request.get_json(force=True)	
+    predict_request = [data['CnaeSession'], 
                        data['Uf'], 
                        data['FundationDate'], 
                        data['Equity']]
-	predict_request = np.array(predict_request)
-	predict_reshape = np.array(predict_request).reshape(1, -1)
+    predict_request = np.array(predict_request)
+    predict_reshape = np.array(predict_request).reshape(1, -1)
 
-	predIndex = modelCatBoost.predict(predict_reshape).astype('int').flatten()
-	predGroup = encoder.inverse_transform(predIndex)
-	## print(predIndex, predGroup)
-	myHardCodePred = 'G1000000'
-	return json.dumps({'hello': myHardCodePred})
-	## return our prediction
-	##return jsonify(predIndex = predIndex.tolist(), predGroup = predGroup.tolist())
-	# return jsonify(yep = "Hello world!")
-	#return jsonify(myGuess = myHardCodePred)
+    predIndex = modelCatBoost.predict(predict_reshape).astype('int').flatten()
+    predGroup = encoder.inverse_transform(predIndex)
+    ## print(predIndex, predGroup)
+    myHardCodePred = 'G1000000'
+    return json.dumps({'hello': myHardCodePred})
+    ## return our prediction
+    ##return jsonify(predIndex = predIndex.tolist(), predGroup = predGroup.tolist())
+    # return jsonify(yep = "Hello world!")
+    #return jsonify(myGuess = myHardCodePred)
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug = True)
