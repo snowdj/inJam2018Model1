28579984e53afb0d7d71dc1342b18365a5c58b00
udothemath1984 <anappleaday1984@gmail.com>
2018-07-26 02:34:04 -0400

    make_prediction

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pickle
 import json
 
 app = Flask(__name__)
-
 @app.route('/')
 def homepage():
     welcomeLabel = """
@@ -13,30 +12,30 @@ def homepage():
     return welcomeLabel
 
 
-# @app.route('/model1', methods=['POST'])
-# def make_predict():
-	# myPickle = 'model.pkl'
-    # encoder, modelCatBoost = pickle.load(open(myPickle, 'rb'))
+@app.route('/model1', methods=['POST'])
+def make_predict():
+	myPickle = 'model.pkl'
+    encoder, modelCatBoost = pickle.load(open(myPickle, 'rb'))
 
-# 	## all kinds of error checking should go here
-# 	data = request.get_json(force=True)
-# 	## convert our json to a numpy array 
-# 	predict_request = [data['CnaeSession'], 
-#                        data['Uf'], 
-#                        data['FundationDate'], 
-#                        data['Equity']]
-# 	predict_request = np.array(predict_request)
-# 	predict_reshape = np.array(predict_request).reshape(1, -1)
+	## all kinds of error checking should go here
+	data = request.get_json(force=True)
+	## convert our json to a numpy array 
+	predict_request = [data['CnaeSession'], 
+                       data['Uf'], 
+                       data['FundationDate'], 
+                       data['Equity']]
+	predict_request = np.array(predict_request)
+	predict_reshape = np.array(predict_request).reshape(1, -1)
 
-# 	predIndex = modelCatBoost.predict(predict_reshape).astype('int').flatten()
-# 	predGroup = encoder.inverse_transform(predIndex)
-# 	## print(predIndex, predGroup)
-# 	myHardCodePred = 'G1000000'
-# 	return json.dumps({'hello': myHardCodePred})
-# 	## return our prediction
-# 	##return jsonify(predIndex = predIndex.tolist(), predGroup = predGroup.tolist())
-# 	# return jsonify(yep = "Hello world!")
-# 	#return jsonify(myGuess = myHardCodePred)
+	predIndex = modelCatBoost.predict(predict_reshape).astype('int').flatten()
+	predGroup = encoder.inverse_transform(predIndex)
+	## print(predIndex, predGroup)
+	myHardCodePred = 'G1000000'
+	return json.dumps({'hello': myHardCodePred})
+	## return our prediction
+	##return jsonify(predIndex = predIndex.tolist(), predGroup = predGroup.tolist())
+	# return jsonify(yep = "Hello world!")
+	#return jsonify(myGuess = myHardCodePred)
 
 if __name__ == '__main__':
 	app.run(debug = True)
