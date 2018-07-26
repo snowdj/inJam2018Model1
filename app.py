e6d4c13f40b4e17867a19f69c91586e050e58a6e
udothemath1984 <anappleaday1984@gmail.com>
2018-07-26 03:14:18 -0400

    124

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def make_predict():
     myPickle = 'model.pkl'
     # encoder, modelCatBoost = pickle.load(open(myPickle, 'rb'))
     
-    encoder, modelCatBoost = joblib.load('model.pkl')
+    ##encoder, modelCatBoost = joblib.load('model.pkl')
     ## all kinds of error checking should go here
     ## convert our json to a numpy array  
     data = request.get_json(force=True)	
@@ -33,7 +33,7 @@ def make_predict():
     # predIndex = modelCatBoost.predict(predict_reshape).astype('int').flatten()
     # predGroup = encoder.inverse_transform(predIndex)
     ## print(predIndex, predGroup)
-    myHardCodePred = 'G123'
+    myHardCodePred = 'G124'
     return json.dumps({'hello': myHardCodePred})
     ## return our prediction
     ##return jsonify(predIndex = predIndex.tolist(), predGroup = predGroup.tolist())
